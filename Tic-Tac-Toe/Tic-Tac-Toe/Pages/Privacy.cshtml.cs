7076ffe97cce765e88dee1832baf9d08a00df3c2
ReginaRakhmaeva <86224557+ReginaRakhmaeva@users.noreply.github.com>
2026-01-03 13:37:29 +0300

    Создание структуры проекта. Веб-приложение ASP.NET Core (Майкрософт)

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tic_Tac_Toe.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
