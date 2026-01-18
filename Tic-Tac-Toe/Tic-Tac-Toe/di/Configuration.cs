83382bd826009bff04b33154516b7f9c2906ada1
ReginaRakhmaeva <86224557+ReginaRakhmaeva@users.noreply.github.com>
2026-01-18 11:16:22 +0500

    класс Configuration описывающий граф зависимостей

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/di/Configuration.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+using Tic_Tac_Toe.datasource.repository;
+using Tic_Tac_Toe.datasource.service;
+using Tic_Tac_Toe.domain.service;
+
+namespace Tic_Tac_Toe.di;
+
+/// Класс для описания графа зависимостей
+public static class Configuration
+{
+    /// Регистрирует все зависимости для работы с играми
+    public static void ConfigureDependencies(IServiceCollection services)
+    {
+        services.AddSingleton<GameStorage>();
+
+        services.AddScoped<IGameRepository, GameRepository>();
+
+        services.AddScoped<IGameService, GameServiceDataSource>();
+    }
+}
